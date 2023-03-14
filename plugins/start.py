037fe53f51990ea94c77a60084bf14cbb31acc88
Razvyyh <razvan.iacobescu@thinkopen.it>
2023-03-14 15:19:51 +0100

    🔄 » Small changes

--- plugins/start.py
+++ plugins/start.py
@@ -6,10 +6,9 @@ from utils import TelegramClient
 
 @Client.on_message(filters.command("start") & filters.private & ~filters.me)
 @Client.on_callback_query(filters.regex("start"))
-async def start(self: TelegramClient, message: Union[Message, CallbackQuery]):
-
-    text: str = f"<b>🏘 Home</b>\n\nWelcome {message.from_user.mention} on {self.me.mention}!"
-    user_id: int = message.from_user.id
+async def start(self: TelegramClient, update: Union[Message, CallbackQuery]):
+    text: str = f"<b>🏘 Home</b>\n\nWelcome {update.from_user.mention} on {self.me.mention}!"
+    user_id: int = update.from_user.id
 
     buttons: list = [
         [
@@ -21,13 +20,13 @@ async def start(self: TelegramClient, message: Union[Message, CallbackQuery]):
         ]
     ]
 
-    if isinstance(message, CallbackQuery):
-        await message.message.edit(text=text, reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
-        await self.update_last_message(message)
+    if isinstance(update, CallbackQuery):
+        await update.message.edit(text=text, reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+        await self.update_last_message(update=update)
     else:
         bot_msg: Message = await self.send_message(chat_id=user_id, text=text,
                                                    reply_markup=InlineKeyboardMarkup(buttons),
                                                    disable_web_page_preview=True)
-        await self.update_last_message(bot_msg)
+        await self.update_last_message(update=bot_msg)
 
-    await self.mysql.check_user(user_id)
+    await self.mysql.check_user(user_id=user_id)
